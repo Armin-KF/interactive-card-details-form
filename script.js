c5413eb0e25d08a633bbea960ec3ca810b140c63
Armin <148791082+Armin-KF@users.noreply.github.com>
2024-09-15 23:48:32 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+ document.addEventListener("DOMContentLoaded", function () {
   let lastClickedElement = null;
 
   function addClickListener(element) {
